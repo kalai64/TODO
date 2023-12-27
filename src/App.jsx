dd30b349a6b21bfae4c47ac33db70bb35ae446eb
kalai64 <kalaikabil6464@gmail.com>
2023-12-27 16:25:32 +0530

    todo-app

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import Topbar from './components/Topbar'
+
+
+
+function App() {
+  let [todo,setTodo]=useState([
+    {
+    id:1,
+    title:" Office Task-1",
+    description:"This is the description for my first task",
+    status:false
+  },
+  {
+    id:2,
+    title:" Office Task-2",
+    description:"This is the description for my second task",
+    status:false   
+  },
+  {
+    id:3,
+    title:" Office Task-3",
+    description:"This is the description for my third task",
+    status:false  
+  }
+])
+let [completed,setCompleted]=useState("All")
+  return <>
+  <Topbar todo={todo} setTodo={setTodo} completed={completed} setCompleted={setCompleted}/>
+  </>
+}
+
+export default App
